a21cf5f649e0b5a5b74f20a3d0f1489325a941b2
Bindholt <Cbindholt@gmail.com>
2023-04-27 13:41:44 +0200

    Committed nyt problem

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ window.addEventListener("load", main);
 function main() {
   console.log("JS virker");
   testFunction();
+  nyPushTing();
 }
 
 function testFunction() {
   alert("hej");
+}
+
+function nyPushTing() {
+  console.log("Fuck jeg elsker problemer");
 }
